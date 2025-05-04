fe23e13d7969570caccc46827394d8aaccb36796
kosalaprasetya <kosalaprasetya@gmail.com>
2025-05-04 23:18:08 +0700

    feat:fix type error on /products

--- src/types/types.ts
+++ src/types/types.ts
@@ -50,6 +50,17 @@ export type ProductType = {
   updatedAt?: Date;
 };
 
+export type ProductCardType = {
+  _id: string;
+  name: string;
+  category: string;
+  producer: {
+    name: string;
+  };
+  price: number;
+};
+
+
 export type NewProduct = {
   name: string;
   description: string;
