62d558f82aa243b074e1dab6fcfd3b6933321f1c
kosalaprasetya <kosalaprasetya@gmail.com>
2025-05-05 09:44:30 +0700

    feat: capitalize user company name and user name

--- src/components/pages/User/UserDashboardPage.tsx
+++ src/components/pages/User/UserDashboardPage.tsx
@@ -18,13 +18,13 @@ const DashboardPage = ({ user }: { user: UserType }) => {
               <div className="space-y-1">
                 <div>
                   <span className="text-xl">Halo, </span>
-                  <span className="text-xl font-bold">{user.name}</span>
+                  <span className="text-xl font-bold capitalize">{user.name}</span>
                 </div>
                 <div className="text-sm">{user.email}</div>
-                <div className="mt-16 text-xl font-bold">{user.companyName}</div>
+                <div className="mt-16 text-xl font-bold capitalize">{user.companyName}</div>
               </div>
               <div className="text-right mt-4 md:mt-0">
-                <div className="text-xl font-bold">{user.role}</div>
+                <div className="text-xl font-bold capitalize">{user.role}</div>
                 <div className="mt-16 text-xl font-bold">{user.contact.address.regency}</div>
                 <div>{user.contact.address.province}</div>
               </div>
