dd089351213ef908f4f830ed826bd3cfb703ce50
kosalaprasetya <kosalaprasetya@gmail.com>
2025-05-05 09:33:55 +0700

    feat: add checkout base page

---
+++ src/app/(client)/(nav-and-footer)/checkout/checkout.tsx
@@ -0,0 +1,12 @@
+import CheckOutPage from '@/components/pages/User/CheckOutPage';
+import React from 'react';
+
+const Checkout = () => {
+  return (
+    <>
+      <CheckOutPage />
+    </>
+  );
+};
+
+export default Checkout;
