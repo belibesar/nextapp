1fb8df815f72c5428b4b63256817b3f321eed219
veryal <ferdiahmdi@gmail.com>
2025-05-07 22:22:41 +0700

    feat: wiring user client to join group buy

--- src/types/types.ts
+++ src/types/types.ts
@@ -95,7 +95,7 @@ export type ProducerType = {
     email: string;
     address: string;
   };
-  products?: ProductType[]
+  products?: ProductType[];
   createdAt?: Date;
   updatedAt?: Date;
 };
@@ -121,26 +121,32 @@ export enum ORDER_STATUS {
 }
 
 export type OrderType = {
-  _id?: string | ObjectId;
-  distributorId?: string | ObjectId;
-  items?: OrderItemType[];
-  totalPrice?: number;
-  currentStatus?: ORDER_STATUS;
-  fullPaymentsStatus?: ORDER_STATUS;
-  isGroupBuy?: boolean;
-  groupBuyId?: string | ObjectId;
-  paymentProof?: string;
-  fullPaymentProof?: string;
-  createdAt?: Date;
-  updatedAt?: Date;
+  distributorId: string | ObjectId;
+  items: OrderItemType;
+  totalPrice: number;
+  currentStatus: ORDER_STATUS;
+  groupBuyId: string | ObjectId;
+  downPayment?: {
+    status: ORDER_STATUS;
+    paymentProof: string;
+    amount: number;
+    percentage: number;
+  };
+  fullPayment?: {
+    status: ORDER_STATUS;
+    paymentProof: string;
+    amount: number;
+  };
+  createdAt: Date;
+  updatedAt: Date;
 };
 
 export type OrderItemType = {
   productId: string | ObjectId;
   quantity: number;
-  price?: number;
-  productName?: string;
-  producerName?: string;
+  // price?: number;
+  // productName?: string;
+  // producerName?: string;
 };
 
 export type GroupBuy = {
@@ -159,8 +165,6 @@ export type GroupBuy = {
   createdAt: Date;
   updatedAt: Date;
   productDetails?: ProductType;
-  productName?: string;
-  price?: number;
 };
 
 export type ParticipantType = {
